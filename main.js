40aa350bbb66046024de3a984bb4280385fb4618
vignesh220402 <skvigneshsivakumar@gmail.com>
2024-11-22 10:59:50 +0530

    route structure

--- main.js
+++ main.js
@@ -5,7 +5,18 @@ const port = 3000
 app.get('/', (req, res) => {
   res.json({msg:"hello"})
 })
+app.get("/movies",(req,res)=>{
 
+})
+app.post("/movies",(req,res)=>{
+    
+})
+app.put("/movies/:id",(req,res)=>{
+    
+})
+app.delete("/movies/:id",(req,res)=>{
+    
+})
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
